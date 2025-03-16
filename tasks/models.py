17959b2ac6c48eb3d68c18daabb047afe5dc21d1
Fabiha Choudhury <fabiha.8@hotmail.co.uk>
2025-03-16 14:52:30 +1100

    Fix static files

--- tasks/models.py
+++ tasks/models.py
@@ -51,8 +51,7 @@ class Task(models.Model):
 
     title = models.CharField(max_length=100, blank=False)
     description = models.CharField(max_length=1000, blank=True)
-    completed = models.BooleanField(default=False)
-    due_date = models.DateField(blank=True, null=True)
+    due_date = models.DateField(blank=False, null=True)
     created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='assigned_to')
